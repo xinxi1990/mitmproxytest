b66102264ebbab3ee733cf0beb813406ee2649fc
xinxi <xinxi@luojilab.com>
2019-04-03 20:38:23 +0800

    first commit

---
+++ venv/lib/python3.7/site-packages/ldap3/version.py
@@ -0,0 +1,12 @@
+# THIS FILE IS AUTO-GENERATED. PLEASE DO NOT MODIFY# version file for ldap3
+# generated on 2018-12-28 22:18:22.295577
+# on system uname_result(system='Windows', node='ELITE10GC', release='10', version='10.0.17763', machine='AMD64', processor='Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
+# with Python 3.7.1 - ('v3.7.1:260ec2c36a', 'Oct 20 2018 14:57:15') - MSC v.1915 64 bit (AMD64)
+#
+__version__ = '2.5.2'
+__author__ = 'Giovanni Cannata'
+__email__ = 'cannatag@gmail.com'
+__url__ = 'https://github.com/cannatag/ldap3'
+__description__ = 'A strictly RFC 4510 conforming LDAP V3 pure Python client library'
+__status__ = '5 - Production/Stable'
+__license__ = 'LGPL v3'
