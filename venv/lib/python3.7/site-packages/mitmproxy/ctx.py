b423b0034038f2670510037c057451a7c44a6afb
xinxi <xinxi@luojilab.com>
2020-02-24 18:09:27 +0800

    add websocket

--- venv/lib/python3.7/site-packages/mitmproxy/ctx.py
+++
@@ -1,7 +0,0 @@
-import mitmproxy.master  # noqa
-import mitmproxy.log  # noqa
-import mitmproxy.options  # noqa
-
-master = None  # type: mitmproxy.master.Master
-log: mitmproxy.log.Log = None
-options: mitmproxy.options.Options = None
